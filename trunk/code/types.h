9407e1f453c9b07a6d049a656a09569e68eec9ab
JohnnyDude <JohnnyDude@f4ef83f8-773f-0410-a807-d18eb2b73810>
2009-03-13 00:00:45 +0000

    more types
    git-svn-id: https://svn.code.sf.net/p/fourdo/code@108 f4ef83f8-773f-0410-a807-d18eb2b73810

--- trunk/code/types.h
+++ trunk/code/types.h
@@ -1,11 +1,13 @@
+// NOTE: I don't understand this, but when I switched to Visual C++ 2005 (from 6.0),
+//       I had to take this typedef out of the inclusion check.
 typedef unsigned int   uint;
 typedef unsigned char  uchar;
 
-// NOTE: I don't understand this, but when I switched to Visual C++ 2005 (from 6.0),
-//       I had to take this typedef out of the inclusion check.
+typedef unsigned char  uint8;
+typedef unsigned short uint16;
+typedef unsigned int   uint32;
+
 #ifndef _INC_TYPES
 #define _INC_TYPES
-
-//typedef unsigned int uint;
 
 #endif //_INC_TYPES
