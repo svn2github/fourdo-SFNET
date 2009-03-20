b19fdf60de84f19b6a4876e91f89ec0633928579
JohnnyDude <JohnnyDude@f4ef83f8-773f-0410-a807-d18eb2b73810>
2009-03-20 02:24:59 +0000

    add inline functions for endianness
    git-svn-id: https://svn.code.sf.net/p/fourdo/code@117 f4ef83f8-773f-0410-a807-d18eb2b73810

--- trunk/code/BitMath.h
+++ trunk/code/BitMath.h
@@ -8,8 +8,38 @@
 #include <cstdio>
 
 //////////////////////////////////////////////////
+// Inline functions
+inline uint32 ByteSwap( uint32 number )
+{
+	return 
+	(
+		( (number & 0x000000FF) << 24 )+
+		( (number & 0x0000FF00) << 8  )+
+		( (number & 0x00FF0000) >> 8  )+
+		( (number & 0xFF000000) >> 24 )
+	);
+}
 
-uint SetBit (uint value, int bitNumber, bool bitValue);
+inline uint32 AlignmentSwap( uint32 number )
+{
+	return 
+	(
+		(number & 0xFFFFFFFC) | 
+		(~(number & 0x00000003) & 0x00000003)
+	);
+}
+
+inline uint32 WordAlign( uint32 number )
+{
+	return 
+	(
+		(number & 0xFFFFFFFC)
+	);
+}
+
+//////////////////////////////////////////////////
+// Normal functions
+uint SetBit  (uint value, int bitNumber, bool bitValue);
 uint SetBits (uint value, uint setMask, uint setValue);
 
 wxString CharToBitString (char value);
